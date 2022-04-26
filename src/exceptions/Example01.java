cec43850d9a350c39e8f1f08026998d72591fe85
DataBorbieva <databorbieva@gmail.com>
2022-04-26 18:36:03 -0500

    added ArraysIndexOutOfBoundsException

---
+++ src/exceptions/Example01.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+public class Example01 {
+    public static void main(String[] args) {
+
+        String[] names = {"Alex", "John", "Max"};
+
+        //How to handle the exception
+        try {
+            System.out.println(names[-5]); //ArrayIndexOutOfBoundsException
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("An ArrayIndexOutOfBoundsException is caught");
+        }
+        System.out.println("End of the program!");
+    }
+}
+
+
