a49c70deace6706797ba6f1d987e4f968dc9352f
DataBorbieva <databorbieva@gmail.com>
2022-04-26 18:45:50 -0500

    added updates

--- src/exceptions/Example01.java
+++ src/exceptions/Example01.java
@@ -10,6 +10,13 @@ public class Example01 {
             System.out.println(names[-5]); //ArrayIndexOutOfBoundsException
         }
         catch (ArrayIndexOutOfBoundsException exception){
+      //      System.out.println(exception.getMessage());
+            exception.printStackTrace();
+            /*
+            printStackTrace() -> java.lang.ArrayIndexOutOfBoundsException: -5
+	at exceptions.Example01.main(Example01.java:10)
+             */
+
             System.out.println("An ArrayIndexOutOfBoundsException is caught");
         }
         System.out.println("End of the program!");
