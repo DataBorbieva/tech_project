95513c4c89f7065787a74c80187ed761c34ad3b5
DataBorbieva <databorbieva@gmail.com>
2022-04-30 11:57:54 -0500

    added enums with for each loop

--- src/date_practices/UnderstandingDateFormat.java
+++ src/date_practices/UnderstandingDateFormat.java
@@ -7,28 +7,28 @@ import java.util.logging.SimpleFormatter;
 
 public class UnderstandingDateFormat {
     public static void main(String[] args) {
+
         Date currentDate = new Date();
-        System.out.println(currentDate); //Sat Apr 30 10:13:42 CDT 2022 -> default format
 
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy"); //lower case "m" stands for minutes
-        System.out.println(sdf.format(currentDate)); //04/30/2022
+        System.out.println(currentDate); // Sat Apr 30 10:15:19 CDT 2022
 
-        SimpleDateFormat sdf1 = new SimpleDateFormat("MM-dd-yyyy"); //04-30-2022
-        System.out.println(sdf1.format(currentDate));
+        // MM/dd/yyyy  04.30.2022
 
-
+        SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yyyy");
+        System.out.println("Today is = " + sdf.format(currentDate));
 
 
-        SimpleDateFormat sdf2 = new SimpleDateFormat("EEE hh:mm a");
-        System.out.println(sdf2.format(currentDate)); //Sat 10:27 AM
+        // Print date in the format of Sat 10:27 AM
+        sdf = new SimpleDateFormat("E hh:mm a");
+        System.out.println(sdf.format(currentDate));
 
-        System.out.println(new SimpleDateFormat("EEEE").format(currentDate));
-        System.out.println(new SimpleDateFormat("yyyy").format(currentDate));
-
-
-
-
-
-
+        System.out.println(new SimpleDateFormat("EEEE").format(currentDate)); // Saturday
+        System.out.println(new SimpleDateFormat("yyyy").format(currentDate)); // 2022
+        System.out.println(new SimpleDateFormat("hh 'o''clock' a, zzzz").format(currentDate)); //
     }
 }
+
+
+
+
+
---
+++ src/enums/Test.java
@@ -0,0 +1,13 @@
+package enums;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Day.MONDAY); //MONDAY
+        System.out.println(Day.SUNDAY); //SUNDAY
+
+        System.out.println(Constants.Gender.FEMALE);
+        System.out.println(Constants.Frequencies.YEARLY);
+    }
+}
